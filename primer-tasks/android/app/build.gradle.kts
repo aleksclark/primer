ded1d8781f0d07c653e1130bd5026483058d2804
Aleks Clark <aleks.clark@gmail.com>
2026-08-19 01:40:19 -0500

    test(android): promote photo picker flow

--- primer-tasks/android/app/build.gradle.kts
+++ primer-tasks/android/app/build.gradle.kts
@@ -45,5 +45,6 @@ dependencies {
  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
  androidTestImplementation("androidx.test.ext:junit:1.2.1")
  androidTestImplementation("androidx.test:runner:1.6.2")
+ androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
  debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
